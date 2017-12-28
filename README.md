56170638852c5184471ff50472f18076deb84353
Josh Patterson <jpatterson@floe.tv>
2017-12-28 14:47:51 -0500

    final code cleanup to publish blog article, one last round of testing

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # SKIL_Examples
 Public examples for the SKIL Platform (client examples, notebooks, etc). Versioned for each release.
+
+##Examples Included in the v1.0.1 Release
+
+* Endpoint Examples
+* MNIST Keras Notebook (python) and Java Client Example
+
+##Running the MNIST Keras Notebook and Model Server Client
+
+Includes a full SKIL application example with a JSON Keras python notebook and an example client in java that base64 encodes MNIST images and sends them to a SKIL Model Server endpoint for classification, returning labels and probabilities.
+
+###Running Model Server Client Code
+
+(from the client_app subdir)
+
+To send a blank image to the model server to test out a non-MNIST image:
+
+java -jar ./target/skil-example-mnist-tf-1.0.0.jar --input blank --endpoint http://localhost:9008/endpoints/mnist/model/mnistmodel/default/
+
+
+A few MNIST digit images (28x28 pngs) are included in the example, and we can send those for classification as well:
+
+java -jar ./target/skil-example-mnist-tf-1.0.0.jar --input ./target/classes/mnist_28x28/3/270.png --endpoint http://localhost:9008/endpoints/mnist/model/mnistmodel/default/
+
+A "3 digit" should return something like:
+
+classification return: {"maxOutcomes":["3"],"rankedOutcomes":[["3","9","8","7","6","5","4","2","1","0"]],"probabilities":[[1,0,0,0,0,0,0,0,0,0]]}
--- skil_mnist_python_app/client_app/src/main/java/ai/skymind/skil/examples/mnist/modelserver/inference/MNISTModelServerInferenceExample.java
+++ skil_mnist_python_app/client_app/src/main/java/ai/skymind/skil/examples/mnist/modelserver/inference/MNISTModelServerInferenceExample.java
@@ -12,32 +12,18 @@ import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.factory.Nd4j;
 import org.nd4j.serde.base64.Nd4jBase64;
 
-//import org.datavec.spark.transform.model.*;
-
-//import org.datavec.image.transform.ImageTransformProcess;
-
 import ai.skymind.skil.examples.mnist.modelserver.inference.model.TransformedImage;
 
 
 import com.mashape.unirest.http.JsonNode;
 import com.mashape.unirest.http.ObjectMapper;
 import com.mashape.unirest.http.Unirest;
-
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
 import org.json.JSONObject;
-
-
 import java.io.IOException;
 
-/*
-import org.apache.commons.io.FileUtils;
-import org.datavec.api.util.ClassPathResource;
-*/
-
-//import ai.skymind.cdg.api.model.Knn;
 import ai.skymind.skil.examples.mnist.modelserver.inference.model.Inference;
-//import ai.skymind.cdg.api.model.TransformedArray;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -57,52 +43,26 @@ import java.util.List;
 
 import ai.skymind.skil.examples.mnist.modelserver.auth.Authorization;
 
-/*
-based on
-https://github.com/deeplearning4j/DataVec/blob/2995fc72b9148e9510fee77e3e1fc7f7671c6ab1/datavec-spark-inference-parent/datavec-spark-inference-server/src/test/java/org/datavec/spark/transform/ImageSparkTransformServerTest.java
-
-
-
-*/
 public class MNISTModelServerInferenceExample {
 
 
-//    @Parameter(names="--transform", description="Endpoint for Transform", required=true)
-    // mnist/model/mnistmodel/default
-    private String skilInferenceEndpoint = "http://localhost:9008/endpoints/mnist/model/mnistmodel/default/";
-    //private String skilInferenceEndpoint = "http://localhost:9601/";
+    @Parameter(names="--endpoint", description="Endpoint for classification", required=true)
+    private String skilInferenceEndpoint = ""; // EXAMPLE: "http://localhost:9008/endpoints/mnist/model/mnistmodel/default/";
 
-
-//    @Parameter(names="--inference", description="Endpoint for Inference", required=true)
-//    private String inferenceEndpoint = "";
-/*
-    @Parameter(names="--type", description="Type of endpoint (multi or single)", required=true)
-    private InferenceType inferenceType;
-*/
     @Parameter(names="--input", description="image input file", required=true)
     private String inputImageFile = "";
-
-
-
-
-//    @Parameter(names="--blank", description="to send a blank image", required=false)
-//    private boolean isBlank = false;
-
-
 
     public void run() throws Exception, IOException {
 
         ImageTransformProcess imgTransformProcess = new ImageTransformProcess.Builder().seed(12345)
             .build();
-
         
         File imageFile = null;
         INDArray finalRecord = null;
 
         if ("blank".equals(inputImageFile)) {
 
-
-            finalRecord = Nd4j.zeros( 1, 28 * 28 ); //imgTransformProcess.executeArray( img ).reshape(1, 28 * 28);
+            finalRecord = Nd4j.zeros( 1, 28 * 28 );
 
             System.out.println( "Generating blank test image ..." );
 
@@ -115,18 +75,14 @@ public class MNISTModelServerInferenceExample {
                 System.exit(2);
             } else {
 
-
                 System.out.println( "Inference for: " + inputImageFile );
 
             }
 
-            
             ImageWritable img = imgTransformProcess.transformFileUriToInput( imageFile.toURI() );
             finalRecord = imgTransformProcess.executeArray( img ).reshape(1, 28 * 28);
 
         }
-
-        //INDArray finalRecord = Nd4j.zeros( 1, 28 * 28 ); //imgTransformProcess.executeArray( img ).reshape(1, 28 * 28);
 
         String imgBase64 = Nd4jBase64.base64String(finalRecord);
 
@@ -134,26 +90,21 @@ public class MNISTModelServerInferenceExample {
 
         System.out.println( "Finished image conversion" );
 
-                // Initialize RestTemplate
-        //RestTemplate restTemplate = new RestTemplate();
-
         skilClientGetImageInference( imgBase64 );
-
-
 
     }
 
     private void skilClientGetImageInference( String imgBase64 ) {
 
-            Authorization auth = new Authorization();
-            String auth_token = auth.getAuthToken( "admin", "admin" );
+        Authorization auth = new Authorization();
+        String auth_token = auth.getAuthToken( "admin", "admin" );
 
-            System.out.println( "auth token: " + auth_token );
+        System.out.println( "auth token: " + auth_token );
 
         try {
 
             String returnVal =
-                    Unirest.post( skilInferenceEndpoint + "multiclassify" ) //MessageFormat.format("http://{0}:{1}/login", "localhost", "9008"))
+                    Unirest.post( skilInferenceEndpoint + "multiclassify" ) 
                             .header("accept", "application/json")
                             .header("Content-Type", "application/json")
                             .header( "Authorization", "Bearer " + auth_token)
@@ -162,7 +113,7 @@ public class MNISTModelServerInferenceExample {
                                     .put("prediction", new JSONObject().put("array", imgBase64))
                                     .toString())
                             .asJson()
-                            .getBody().getObject().toString(); //.getString("token");
+                            .getBody().getObject().toString(); 
 
 
             System.out.println( "classification return: " + returnVal );
@@ -170,39 +121,6 @@ public class MNISTModelServerInferenceExample {
         } catch (UnirestException e) {
             e.printStackTrace();
         }
-
-/*
-            RestTemplate restTemplate = new RestTemplate();
-
-            final HttpHeaders requestHeaders = new HttpHeaders();
-            final Object inferenceRequest = new Inference.Request( imgBase64.toString() );
-
-
-            final HttpEntity<Object> httpEntity =
-                    new HttpEntity<Object>(inferenceRequest, requestHeaders);
-
-                // Accept JSON
-                requestHeaders.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
-
-                // Temp fix
-                List<HttpMessageConverter<?>> converters = restTemplate.getMessageConverters();
-                converters.add(new ExtendedMappingJackson2HttpMessageConverter());
-                restTemplate.setMessageConverters(converters);
-
-
-
-            Class clazz = Inference.Response.Classify.class; // : Inference.Response.MultiClassify.class;
-
-            //Object request = new Inference.Request( imgBase64.toString() );
-
-             final Object response = restTemplate.postForObject(
-                         skilInferenceEndpoint + "classify",
-                         inferenceRequest,
-                         clazz);
-
-             System.out.format("Inference response: %s\n", response.toString());
-*/
-
 
     }
 
@@ -215,13 +133,5 @@ public class MNISTModelServerInferenceExample {
           .parse(args);
 
         m.run();
-    }
-}
-
-class ExtendedMappingJackson2HttpMessageConverter extends MappingJackson2HttpMessageConverter {
-    public ExtendedMappingJackson2HttpMessageConverter() {
-        List<MediaType> types = new ArrayList<MediaType>(super.getSupportedMediaTypes());
-        types.add(new MediaType("text", "plain", DEFAULT_CHARSET));
-        super.setSupportedMediaTypes(types);
     }
 }
